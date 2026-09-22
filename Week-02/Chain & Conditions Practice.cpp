6263b5f52f4e4ede6f38fcabc8741068bf795237
lmmp025-max <lmmp025@gmail.com>
2026-09-21 23:50:50 -0400

    add C++ practice program

---
+++ Week-02/Chain & Conditions Practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a = 20.39;
+    if(a>=10){
+        if(a!=15){
+            if(a==20){
+                cout<<"yippe";
+            }
+        }
+    }
+
+    return 0;
+}
